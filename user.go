19a0d05c86399fc32c88dd1737217e0c3b27ff06
n8wb <vanish172@gmail.com>
2020-04-08 12:55:03 -0700

    add the user struct

---
+++ user.go
@@ -0,0 +1,17 @@
+/**
+ * Copyright 2020 Innodev LLC. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
+package common
+
+import(
+	"database/sql"
+)
+
+type User struct {
+	ID string `db:"id"`
+	GithubUsername sql.NullString `db:"github_username"`
+	Plan string `db:"plan"`
+}
