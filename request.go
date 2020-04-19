a20580d0c241536c9d418aa83da6a93fd2e3b562
n8wb <vanish172@gmail.com>
2020-04-19 14:37:20 -0700

    add more to the request object

--- request.go
+++ request.go
@@ -12,18 +12,19 @@ import (
 )
 
 type Request struct {
-	ID         string    `json:"id,omitempty" cli:"id,omitempty"`
-	Timestamp  time.Time `json:"timestamp,omitempty" cli:"timestamp,omitempty"`
-	URL        string    `json:"url,omitempty" cli:"url,omitempty"`
-	Version    string    `json:"version,omitempty" cli:"version,omitempty"`
-	Hash       string    `json:"hash,omitempty" cli:"hash,omitempty"`
-	VCS        string    `json:"vcs,omitempty" cli:"vcs,omitempty"`
-	source     string
+	ID         string                 `json:"id,omitempty" cli:"id,omitempty"`
+	Timestamp  time.Time              `json:"timestamp,omitempty" cli:"timestamp,omitempty"`
+	URL        string                 `json:"url,omitempty" cli:"url,omitempty"`
+	Version    string                 `json:"version,omitempty" cli:"version,omitempty"`
+	Hash       string                 `json:"hash,omitempty" cli:"hash,omitempty"`
+	VCS        string                 `json:"vcs,omitempty" cli:"vcs,omitempty"`
+	Source     string                 `json:"-" cli:"-"`
 	Name       string                 `json:"name,omitempty" cli:"name,omitempty"`
 	NormalName string                 `json:"-,omitempty" cli:"-,omitempty"`
 	Meta       map[string]interface{} `json:"meta,omitempty" cli:"meta,omitempty"`
 	UID        string                 `json:"uid,omitempty" cli:"user,omitempty"`
 	PR         PRContext
+	AckFn      func() `json:"-"`
 }
 
 func (req Request) MarshalFirebase() (map[string]interface{}, error) {
@@ -38,4 +39,34 @@ func (req Request) MarshalFirebase() (map[string]interface{}, error) {
 		return nil, err
 	}
 	return out, nil
+}
+
+func NewRequestFromModule(mod Module) Request {
+	return Request{
+		URL:        mod.GetURL(),
+		Version:    mod.Version,
+		Hash:       mod.Hash,
+		VCS:        mod.GetVCS(),
+		Source:     mod.Source,
+		Name:       mod.Name,
+		NormalName: mod.NormalName,
+	}
+}
+
+func (req Request) ToModuleShell() Module {
+	return Module{
+		Name:       req.Name,
+		NormalName: req.NormalName,
+		URL:        req.URL,
+		Version:    req.Version,
+		Hash:       req.Hash,
+		VCS:        req.VCS,
+		Source:     req.Source,
+	}
+}
+
+func (req Request) Ack() {
+	if req.AckFn != nil {
+		req.AckFn()
+	}
 }
